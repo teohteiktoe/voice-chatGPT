7409b0c9e6f0e56ced3e90fdc7848b693721ce85
Teoh TeikToe <ttt@mensa.org.sg>
2023-03-26 20:37:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import json
 app = Flask(__name__)
 
 # Set OpenAI API key and model
-openai.api_key = os.getenv('openai-key')
+openai.api_key = "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"
 
 from google.cloud import speech_v1p1beta1 as speech
 
